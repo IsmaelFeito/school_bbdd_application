f42b3a6460801e47c48aa5790ba0eae2128aa0d3
Ismael <isfemo15@gmail.com>
2025-03-22 00:46:25 +0100

    enlazadas sesrvers(local+ernesto)

--- src/ernesto/Ernesto.java
+++ src/ernesto/Ernesto.java
@@ -3,8 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ernesto;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
+/*
  *
  * @author ifeito-m
  */
@@ -13,8 +20,38 @@ public class Ernesto {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-	// TODO code application logic here
+    public static void main(String[] args) throws ClassNotFoundException {
+
+	try {
+	    //charge MariaDB's driver
+	    Class.forName("org.mariadb.jdbc.Driver");
+	    //clase
+//	    Connection connection = DriverManager.getConnection("jdbc:mariadb://10.227.189.195:3307/alberto_isma", "root", "alumno");
+	    
+	    //local:
+	    Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost/ernesto?socket=/run/mysqld/mysqld.sock", "root", "8001");
+	    //create statement (sentencia)
+	    Statement stmt = connection.createStatement();
+	    //save query's text on a str (after will exe it)
+	    String query = "SELECT * FROM Ciclos";
+            ResultSet rs = stmt.executeQuery(query);
+	    
+	    
+	    System.out.println("CicloID  - Ciclos");
+	    while (rs.next()){
+		System.out.println(rs.getString("CicloID")+"        - "+rs.getString("NombreCiclo"));
+	    }
+	    
+	    rs.close();
+	    stmt.close();
+	    connection.close();
+	} catch (ClassNotFoundException e){
+	    System.out.println("Error: MariaDB's driver not found");
+	    e.printStackTrace();
+	} catch (SQLException e) {
+	    System.out.println("Error: connection with Database failed");
+            e.printStackTrace();
+        }
     }
     
 }
